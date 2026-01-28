3fe4fb00043fc049eeea67fa302277d61ffb3c02
owolabi-develop <owolabidevelop84@gmail.com>
2026-01-28 20:28:27 +0600

    update to react router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.tsx";
@@ -90,7 +90,7 @@ createRoot(document.getElementById("root")!).render(
           <Route key={r.path} path={r.path} element={r.element} />
         ))}
 
-        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
